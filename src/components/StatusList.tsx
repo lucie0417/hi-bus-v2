f98dd3d8a671a2eec3586324571fbc8d8ac65aaa
Lucie <ipeicram0417@gmail.com>
2024-07-07 23:47:43 +0800

    bus Status css

--- src/components/StatusList.tsx
+++ src/components/StatusList.tsx
@@ -7,8 +7,8 @@ const StatusList = () => {
 	return (
 		<ul className='px-3 divide-y divide-gray-light text-dark-blue md:px-10'>
 			{busStopInfo[0].Stops.map((stop: any) => (
-				<li className='flex items-center py-3'>
-					<Badge className='px-4 py-2 bg-main-red/30 text-sm text-main-red font-medium'>
+				<li className='relative flex items-center py-3'>
+					<Badge className='px-4 py-2 bg-main-red/30 text-sm text-main-red font-medium hover:bg-main-red/30'>
 						進站中
 					</Badge>
 					<span className='ml-2'>{stop.StopName.Zh_tw}</span>
@@ -19,13 +19,18 @@ const StatusList = () => {
 						<span className='text-gray-dark font-english font-light'>712-SW</span>
 					</Badge>
 
-					<div className='relative'>
-						<div className='absolute right-1/2 bottom-1/4 w-0.5 h-12 bg-gray-light'></div>
-
-						<span className='relative flex h-2.5 w-2.5'>
-							<span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-main-green opacity-75'></span>
-							<span className='relative inline-flex rounded-full h-2.5 w-2.5 bg-main-green'></span>
+					<div className='absolute right-[3px] top-1/2 w-0.5 h-16 bg-gray-default'></div>
+					<div className='absolute right-0'>
+						{/* 未到站 */}
+						<span className='relative'>
+							<span className='absolute rounded-full w-2 h-2 bg-gray-default right-0'></span>
+							<span className='absolute rounded-full h-1 w-1 bg-white right-[2px] top-[2px]'></span>
 						</span>
+						{/* 到站 */}
+						{/* <span className='relative flex h-2 w-2'>
+							<span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-main-blue opacity-75'></span>
+							<span className='relative inline-flex rounded-full h-2 w-2 bg-main-blue'></span>
+						</span> */}
 					</div>
 				</li>
 			))}
